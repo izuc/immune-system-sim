f254924f664925b02ff8aed2a4a9e21256e8924e
izuc <the@lance.name>
2025-02-24 18:27:43 +1100

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    modulePreload: {
+      polyfill: true
+    },
+    rollupOptions: {
+      input: 'index.html'
+    }
+  },
+  server: {
+    port: 5173,
+    strictPort: false
+  }
 })
